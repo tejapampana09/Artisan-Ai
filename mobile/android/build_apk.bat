18c3f67a9e1cba7759a6d5a7f7edfe144822cb1e
TEJA PAMPANA <tejapampana36@gmail.com>
2026-09-23 01:59:44 +0530

    chore(mobile): remove obsolete local build_apk.bat script

--- mobile/android/build_apk.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.20.101-hotspot"
-set "ANDROID_HOME=C:\Users\tejap\AppData\Local\Android\Sdk"
-set "PATH=%JAVA_HOME%\bin;%ANDROID_HOME%\platform-tools;%PATH%"
-echo Starting Artisan AI Release APK Build...
-echo JAVA_HOME: %JAVA_HOME%
-echo ANDROID_HOME: %ANDROID_HOME%
-call gradlew.bat assembleRelease --no-daemon
